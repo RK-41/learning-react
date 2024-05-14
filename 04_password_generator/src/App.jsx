eb50797b4a7219c93e3262dc0556f7ec6b9e5bc7
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2024-05-15 01:28:24 +0530

    Updated the UI of the web app

--- 04_password_generator/src/App.jsx
+++ 04_password_generator/src/App.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 function App() {
 	const [length, setLength] = useState(8);
@@ -7,6 +7,7 @@ function App() {
 	const [includeSpecialCharacters, setIncludeSpecialCharacters] =
 		useState(false);
 	const [password, setPassword] = useState('pass');
+	const passwordRef = useRef(null);
 
 	const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 	const numbers = '0123456789';
@@ -32,72 +33,82 @@ function App() {
 
 	const copyPassword = () => {
 		window.navigator.clipboard.writeText(password);
+		passwordRef.current.select();
 	};
 
 	return (
 		<div className='items-center text-lg'>
-			<h1 className='text-5xl text-center my-10'>Password Generator</h1>
+			<h1 className='text-5xl text-blue-500 text-center p-4 my-20 w-[520px] rounded-lg mx-auto shadow-xl shadow-blue-500'>
+				Password Generator
+			</h1>
 
 			{/* Password */}
-			<div className='flex my-10 bg-yellow-300 w-[400px] mx-auto rounded-md'>
+			<div className='flex my-10 bg-blue-500 w-[440px] mx-auto rounded-lg shadow-lg shadow-blue-500'>
 				<input
 					type='text'
 					name='Password'
 					value={password}
 					readOnly
-					className='border-blue-500 border-2 p-1 w-[80%] rounded-l-md text-center cursor-pointer outline-none'
+					ref={passwordRef}
+					className='border-blue-500 border-2 p-1 w-[80%] rounded-l-lg text-center cursor-pointer outline-none selection:bg-[#33ff33]'
 				></input>
-				<button onClick={copyPassword} className='w-[20%]'>
+				<button onClick={copyPassword} className='w-[20%] text-[#ffff33]'>
 					Copy
 				</button>
 			</div>
 
 			{/* Length */}
-			<div className='flex justify-center items-center gap-3 m-3 select-text bg-yellow-300 w-[400px] h-10 mx-auto rounded-lg'>
+			<div className='flex justify-center items-center gap-3 m-3 select-text bg-[#ffff33] w-[440px] h-10 mx-auto rounded-lg shadow-md shadow-yellow-400'>
+				<label htmlFor='length' className='w-24'>
+					Length: {length}
+				</label>
 				<input
 					type='range'
 					min={6}
-					max={20}
+					max={30}
 					name='length'
 					id='length'
 					value={length}
 					onChange={(e) => setLength(e.target.value)}
+					className='cursor-pointer'
 				/>
-				<label htmlFor='length'>Length: {length}</label>
 			</div>
 
 			{/* Numbers */}
-			<div className='flex justify-center items-center gap-3 m-3 bg-yellow-300 w-[400px] h-10 mx-auto rounded-lg'>
+			<div className='flex justify-center items-center gap-3 m-3 bg-[#ffff33] w-[440px] h-10 mx-auto rounded-lg shadow-md shadow-yellow-400 cursor-pointer'>
 				<input
 					type='checkbox'
 					value={includeNumbers}
 					name='includeNumbers'
 					id='includeNumbers'
 					onClick={() => setIncludeNumbers((prev) => !prev)}
-					className='size-4'
+					className='size-4 cursor-pointer'
 				/>
-				<label htmlFor='includeNumbers' className='select-none'>
+				<label htmlFor='includeNumbers' className='select-none cursor-pointer'>
 					Include Numbers
 				</label>
 			</div>
 
 			{/* Special Characters */}
-			<div className='flex justify-center items-center gap-3 m-3 bg-yellow-300 w-[400px] h-10 mx-auto rounded-md'>
+			<div className='flex justify-center items-center gap-3 m-3 bg-[#ffff33] w-[440px] h-10 mx-auto rounded-lg shadow-md shadow-yellow-400 cursor-pointer'>
 				<input
 					type='checkbox'
 					value={includeSpecialCharacters}
 					name='includeSpecialCharacters'
 					id='includeSpecialCharacters'
 					onClick={() => setIncludeSpecialCharacters((prev) => !prev)}
-					className='size-4'
+					className='size-4 cursor-pointer'
 				/>
-				<label htmlFor='includeSpecialCharacters' className='select-none'>
+				<label
+					htmlFor='includeSpecialCharacters'
+					className='select-none cursor-pointer'
+				>
 					Include Special Characters
 				</label>
 			</div>
 
 			<button
-				className='flex justify-center items-center bg-blue-500 w-[400px] h-10 mx-auto rounded-md'
+				className='flex justify-center items-center bg-[#33ff33] w-[440px] h-10 mx-auto rounded-lg shadow-md shadow-green-500'
 				onClick={generatePassword}
 			>
 				Generate Random Password
