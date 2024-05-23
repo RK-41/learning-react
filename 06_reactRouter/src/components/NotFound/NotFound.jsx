eb410c651f32482505d5c341f9dd05e9422af823
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2024-05-23 22:46:26 +0530

    Minor tweak

--- 06_reactRouter/src/components/NotFound/NotFound.jsx
+++ 06_reactRouter/src/components/NotFound/NotFound.jsx
@@ -1,6 +1,6 @@
 function NotFound() {
 	return (
-		<div className='text-center mt-10 text-4xl text-blue-600 font-bold'>
+		<div className='text-center mt-20 text-4xl text-blue-600 font-bold'>
 			Error 404: Not Found
 		</div>
 	);
