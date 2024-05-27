38053c8ad500f31820412894877c550ef6e406db
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2024-05-27 23:58:17 +0530

    Learning the use of Context API and its application through a demo Login Form

---
+++ 07_contextAPI/src/components/Login.jsx
@@ -0,0 +1,50 @@
+import { useState, useContext } from 'react';
+import UserContext from '../context/UserContext';
+
+function Login() {
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+
+	const { setUser } = useContext(UserContext);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		if (username != '' && password != '') {
+			setUser({ username, password });
+		} else {
+			alert('Invalid username or password');
+		}
+		console.log({ username, password });
+		setUsername('');
+		setPassword('');
+	};
+
+	return (
+		<div>
+			<h2>Login</h2>
+
+			<form id='login-form'>
+				<input
+					type='text'
+					name='username'
+					id='usename'
+					value={username}
+					onChange={(e) => setUsername(e.target.value)}
+					placeholder='username'
+				/>
+				<input
+					type='password'
+					name='password'
+					id='password'
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+					placeholder='password'
+				/>
+				<button onClick={handleSubmit}>Login</button>
+			</form>
+		</div>
+	);
+}
+
+export default Login;
