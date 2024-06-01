914e3b71622215361396b3420cb1f05b141d8c01
RAJAN KUMAR <73783957+RK-41@users.noreply.github.com>
2024-06-02 00:21:34 +0530

    Defined functionality related to to-do addition, updation, deletion, completion, fetching from local storage and updating local storage

--- 09_todoApp/src/App.jsx
+++ 09_todoApp/src/App.jsx
@@ -1,8 +1,50 @@
+import { useEffect, useState } from 'react';
+import { TodoContextProvider } from './contexts';
+
 function App() {
+	const [todos, setTodos] = useState([]);
+
+	const addTodo = (todo) => {
+		setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
+	};
+
+	const updateTodo = (id, updatedTodo) => {
+		setTodos((prev) =>
+			prev.map((todo) => (todo.id === updatedTodo.id ? updatedTodo : todo))
+		);
+	};
+
+	const deleteTodo = (id) => {
+		setTodos((prev) => prev.filter((todo) => todo.id !== id));
+	};
+
+	const toggleCompleted = (id) => {
+		setTodos((prev) =>
+			prev.map((todo) =>
+				todo.id === id ? { ...todo, completed: !todo.completed } : { todo }
+			)
+		);
+	};
+
+	// Fetching todos from local storgage when App component is mounted
+	useEffect(() => {
+		const todos = JSON.parse(localStorage.getItem('todos'));
+		if (todos && todos.length) {
+			setTodos(todos);
+		}
+	}, []);
+
+	// Updating local storage when todos array is updated
+	useEffect(() => {
+		localStorage.setItem('todos', JSON.stringify(todos));
+	}, [todos]);
+
 	return (
-		<>
+		<TodoContextProvider
+			value={{ todos, addTodo, updateTodo, deleteTodo, toggleCompleted }}
+		>
 			<h1 className='text-5xl'>Todo App</h1>
-		</>
+		</TodoContextProvider>
 	);
 }
 
